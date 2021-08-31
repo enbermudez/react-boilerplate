4df001a0f71b1e9398914bfbdd8d0447fcaa9f26
Enrique Bermúdez <enrique.bermudez@mercadolibre.com>
2021-08-31 18:53:53 -0300

    feat: add base skeleton

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h2>React Boilerplate</h2>
+      <h3>Date: {new Date().toDateString()}</h3>
+    </div>
+  );
+}
+
+export default App;
